27a7c3e63d8b766676c541a30b81a07d3685b803
aowubulao <635416616@qq.com>
2020-06-11 22:26:42 +0800

    Remove unused code

--- src/main/java/com/neoniou/com/mail/Base64Util.java
+++ src/main/java/com/neoniou/com/mail/Base64Util.java
@@ -10,22 +10,12 @@ public class Base64Util {
 
     /**
      * Encode content
+     *
      * @param code content
      * @return encoded content
      */
     public static String encode(String code) {
         Base64.Encoder encoder = Base64.getEncoder();
         return encoder.encodeToString(code.getBytes());
-    }
-
-    /**
-     * Decode content
-     * @param code content
-     * @return decoded content
-     */
-    public static String decode(String code) {
-        Base64.Decoder decoder = Base64.getDecoder();
-        byte[] bytes = decoder.decode(code);
-        return Arrays.toString(bytes);
     }
 }
